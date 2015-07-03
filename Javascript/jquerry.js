3d5cc5370d28059d2b82004ffdcfd93e2bb8b489
PaulCristian <paul_d007@yahoo.com>
2015-07-03 18:45:57 +0300

    first commit

---
+++ Javascript/jquerry.js
@@ -0,0 +1,32 @@
+// JavaScript Document
+
+$(document).ready(function() {
+    $('.pics').cycle({
+		fx: 'scrollDown'// choose your transition type, ex: fade, scrollUp, shuffle, etc...
+	});
+});
+
+
+$(document).ready(function() {
+    $('.pics2').cycle({
+		fx: 'scrollDown',
+		delay:3700// choose your transition type, ex: fade, scrollUp, shuffle, etc...
+	});
+});
+
+
+$(document).ready(function() {
+    $('.pics3').cycle({
+		fx: 'scrollDown',
+		delay:2700// choose your transition type, ex: fade, scrollUp, shuffle, etc...
+	});
+});
+
+$(document).ready(function() {
+    $('.tot').cycle({
+		fx: 'scrollDown',
+		delay:2700// choose your transition type, ex: fade, scrollUp, shuffle, etc...
+	});
+});
+
+
